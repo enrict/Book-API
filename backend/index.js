79f9506a8550b4d8d90f3149968e59981ce70f5b
enrict <enric_trillo@outlook.com>
2020-10-08 09:16:31 +0100

    configuring the server and middleware

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,28 @@
 // importing the express package
 let express = require("express");
+let morgan = require("morgan");
+let multer = require("multer");
+let path = require("path");
+
+// let express = require("express");
 
 // initialising the server
 const server = express();
 
 // setting the server port
 server.set("port", 3000);
+
+// middleware
+server.use(morgan("dev"));
+let storage = multer.diskStorage({
+  destination: path.join(__dirname, "public/uploads"),
+  filename(req, file, cb) {
+    cb(null, new Date().getTime() + path.extname(file.originalname)); // gets the date as numbers with original extension
+  },
+});
+server.use(multer({ storage }).single("image"));
+server.use(express.urlencoded({ extended: false }));
+server.use(express.json());
 
 // starting the server
 server.listen(server.get("port"), () => {
